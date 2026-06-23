33636e63dc58814a62577ac9f047c631bf220301
Lawal <fatimaadewumi877@gmail.com>
2026-06-23 15:34:07 -0700

    frist commit

---
+++ layout.js
@@ -0,0 +1,14 @@
+function login() {
+
+let email = document.getElementById("email").value;
+let password = document.getElementById("password").value;
+
+if(email === "admin@gmail.com" && password === "123456"){
+    alert("Login Successful");
+    window.location.href = "dashboard.html";
+}
+else{
+    alert("Invalid Email or Password");
+}
+
+}
